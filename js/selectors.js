5971d9a59a92dead8f2f09b3db51e2a09b99a982
Julian M. Qapo <julian.qapo@gmail.com>
2019-01-14 22:26:16 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,19 @@
 function paragraphSelector(){
 return  ${'p'}
 }
+
+function lastImageSelector(){
+  return $("img:last")
+}
+
+function ninjaBabySelector(){
+  return $('#pic-list')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('li:first')
+}
